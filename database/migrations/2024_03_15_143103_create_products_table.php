5129cf0199c58afd17c526fe278c738063e5dc05
EmanueleVenditti95 <vendittiema@gmail.com>
2024-03-15 15:35:57 +0100

    first commit

---
+++ database/migrations/2024_03_15_143103_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->name();
+            $table->description();
+            $table->image_url();
+            $table->ean_code();
+            $table->price();
+            $table->highlighted();          
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
