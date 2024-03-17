d4d9dd0733fe9c8ca480eb0164706da6494395ca
EmanueleVenditti95 <vendittiema@gmail.com>
2024-03-17 17:44:04 +0100

    Add - product seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,16 +2,28 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ProductSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i=0; $i<100; $i++) {
+            $new_product = new Product();
+            $new_product->name = $faker->sentence(2);
+            $new_product->description = $faker->text(50);
+            $new_product->image_url = $faker->imageUrl(360, 360,true);
+            $new_product->ean_code = $faker->ean13();
+            $new_product->cost = $faker->randomFloat(2,10,100);
+            $new_product->highlighted = ($i<5) ? true : false;
+            $new_product->category_id = $faker->numberBetween(1, 5);
+            $new_product->save();
+        }
     }
 }
