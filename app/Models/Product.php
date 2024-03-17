9282177d252adcd388f8f3f9be1187ae6b756baf
EmanueleVenditti95 <vendittiema@gmail.com>
2024-03-17 17:14:49 +0100

    Add - model category and product with relations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+        'image_url',
+        'ean_code',
+        'cost',
+        'highlighted',
+        'category_id'
+    ];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
 }
