0215b2ce238687c28f8056cf9b889c2d32a3782e
EmanueleVenditti95 <vendittiema@gmail.com>
2024-03-18 15:27:22 +0100

    fix home view

--- resources/views/products/home.blade.php
+++ resources/views/products/home.blade.php
@@ -1,30 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Products</title>
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+@extends('layouts.app')
 
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-</head>
-<body>
-    <main>
-        @foreach ($products as $product)
-            <ul>
-                @foreach ($categories as $category)  
-                    @if ($product->category_id == $category->id)
-                        <li>{{$category->name}}</li>                    
-                    @endif                  
-                @endforeach
-                <li>{{ $product->name }}</li>
-                <li>{{ $product->description }}</li>
-                <li>${{ $product->cost }}</li>    
-            </ul>
-        @endforeach
-    </main>
-</body>
-</html>
+@section('content')
+    @foreach ($products as $product)
+        <ul>
+            <li>{{ $product->category->name }}</li>
+            <li>{{ $product->name }}</li>
+            <li>{{ $product->description }}</li>
+            <li>${{ $product->cost }}</li>
+        </ul>
+    @endforeach
+@endsection
