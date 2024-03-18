e39a0b082acb7d482ec67f3acc993c4624e7541c
EmanueleVenditti95 <vendittiema@gmail.com>
2024-03-18 16:10:09 +0100

    add - layout card in home

--- resources/views/products/home.blade.php
+++ resources/views/products/home.blade.php
@@ -1,12 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach ($products as $product)
-        <ul>
-            <li>{{ $product->category->name }}</li>
-            <li>{{ $product->name }}</li>
-            <li>{{ $product->description }}</li>
-            <li>${{ $product->cost }}</li>
-        </ul>
-    @endforeach
+    <div class="container">
+        <div class="row gap-3">
+            @foreach ($products as $product)
+                <ul class="card col-2 d-flex-column align-items-center">
+                    <li>{{ strtoupper($product->category->name) }}</li>
+                    <li>${{ $product->cost }}</li>
+                    <li class="fw-bold fs-5">{{ ucwords($product->name) }}</li>
+                    <li class="fs-6 fw-light text-center">{{ $product->description }}</li>
+                </ul>
+            @endforeach
+        </div>
+    </div>
+
+
+
+    <style lang="scss" scoped>
+        .card {
+            border: none;
+            background-color: black;
+            color: white;
+            
+        }
+    </style>
 @endsection
